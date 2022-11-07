0003dd3c92de12411f215635f945bd69cc30e81c
Daniel_juarez97 <juarezz.daniel97@gmail.com>
2022-11-07 11:26:00 -0300

    configuracion proyecto

--- proyecto_parcial2/settings.py
+++ proyecto_parcial2/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'proyecto_parcial2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'parcial',
+    'USER': 'postgres',
+    'PASSWORD': 'danieljuarez2021',
     }
 }
 
